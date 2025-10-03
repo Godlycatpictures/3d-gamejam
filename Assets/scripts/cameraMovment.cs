2ed6da46c2ef0342845d296522bbbdeef18124d2
xXminimannenXx <minimannen06@gmail.com>
2025-10-04 00:13:31 +0200

    kamera movment klar

---
+++ Assets/scripts/cameraMovment.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class cameraMovment : MonoBehaviour
+{
+
+    [SerializeField] private float senseX, senseY;
+
+    [SerializeField] private Transform orientation;
+
+    private float xRotation, yRotation;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senseX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senseY;
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
