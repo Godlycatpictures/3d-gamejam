9e523aa3628d6a1a44be4173903af4e169fe1081
xXminimannenXx <minimannen06@gmail.com>
2025-10-04 17:07:46 +0200

    timer script
    timer med functioner att pausa, starta och börja om timern

---
+++ Assets/scripts/timerManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class timerManager : MonoBehaviour
+{
+    public float timer = 0f;
+    public bool isRunning = false;
+
+    public void StartTimer()
+    {
+        isRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+    public void ResetTimer()
+    {
+        timer = 0f;
+    }
+    void Update()
+    {
+        if (isRunning)
+        {
+            timer += Time.deltaTime;
+        }
+        // There are 600 seconds in 10 minutes (10 * 60 = 600)
+        if (timer >= 600f)
+        {
+            isRunning = false;
+            Debug.Log("Timer reached 10 minutes!");
+            //if qutoa = klar + man är på plats = ingen punishment else punishment
+            //reset timer
+            //ny dag etc
+        }
+    }
+
+}
