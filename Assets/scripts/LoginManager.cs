3a20cfe1050c45d865a95457f781553d02da31d5
Kannibal Gebis <rexmutter4@gmail.com>
2025-10-09 13:57:13 +0200

    loginsound

--- Assets/scripts/LoginManager.cs
+++ Assets/scripts/LoginManager.cs
@@ -6,7 +6,12 @@ public class LoginManager : MonoBehaviour
     [SerializeField] private string inputText;
     [SerializeField] private string correctPassword = "opensesame";
     [SerializeField] private GameObject loggingInText, incorrectPasswordText, loggedIn;
+    public AudioSource audioSource;
+    public AudioClip loginSound;
+    public AudioClip loginFailSound;
     public TMP_InputField inputField;
+
+    public bool stopLogic;
 
     private void Start()
     {
@@ -20,22 +25,30 @@ public class LoginManager : MonoBehaviour
     }
     public void CheckPassword(string input)
     {
+        if (!stopLogic) { 
         if (input == correctPassword.ToLower())
         {
+            stopLogic = true;
             loggingInText.SetActive(true);
             incorrectPasswordText.SetActive(false);
             Debug.Log("Password is correct.");
             StartCoroutine(Quarantine(4));
+            audioSource.PlayOneShot(loginSound);
+
         }
         else
         {
+            audioSource.PlayOneShot(loginFailSound);
             incorrectPasswordText.SetActive(true);
             Debug.Log("Password is incorrect.");
+
         }
+    }
     }
     IEnumerator Quarantine(int seconds)
     {
         yield return new WaitForSecondsRealtime(seconds);
+        
         Destroy(this.gameObject);
         loggedIn.SetActive(true);
     }
