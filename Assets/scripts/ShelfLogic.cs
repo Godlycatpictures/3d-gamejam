bc9a24972f7db79609856d526349201036b6c432
Godlycatpictures <129548385+Godlycatpictures@users.noreply.github.com>
2025-10-09 20:21:03 +0200

    behöver lösa koden för att öppna shelf

--- Assets/scripts/ShelfLogic.cs
+++ Assets/scripts/ShelfLogic.cs
@@ -13,6 +13,7 @@ public class ShelfLogic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        HasShelfKey = false;
         anim = GetComponent<Animator>();
         playerInteract = FindFirstObjectByType<playerInteract>();
     }
